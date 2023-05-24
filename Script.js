0d6013be8b801a0b82face28819b1d99caf1f880
berumotto2 <raffraymaxime@hotmail.fr>
2023-05-24 21:17:23 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,4 +1,4 @@
-const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+const sounds = ["squeex_laughing", "boo", "gasp", "tada", "victory", "wrong"];
 
 sounds.forEach((sound) => {
   const btn = document.createElement("button");
